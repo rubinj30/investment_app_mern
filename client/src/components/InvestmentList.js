50f59646a7feb039b79193d4ae34a1e1c90711ec
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-02-25 15:44:02 -0500

    styling with fake price data

--- client/src/components/InvestmentList.js
+++ client/src/components/InvestmentList.js
@@ -7,7 +7,9 @@ import NewInvestment from './NewInvestment'
 class InvestmentList extends Component {
     state = {
         user: {},
-        investments: []
+        investments: [],
+        fakePrices: ['23', '122', '1,2231', '12', '21'],
+        fakeTotals: ['1,000.44', '2,000.22', '12,500.12', '1,234.00', '9,928.21']
     }
 
     componentWillMount = async () => {
@@ -30,21 +32,44 @@ class InvestmentList extends Component {
     render() {
         return (
             <div>
-                    <Table>
-                        <Column>
+                <Table>
 
-                            {this.state.investments.map(investment => (
-                                <div>
+                    <Column>
+                        <ColumnTitle>ticker</ColumnTitle>
 
-                                    <Link key={investment._id} to={`/users/${this.props.match.params.id}/investments/${investment._id}`}>
-                                        {investment.ticker}
-                                    </Link>
+                        {this.state.investments.map(investment => (
+                            <div>
 
-                                </div>
-                            ))}
-                        </Column>
+                                <Link key={investment._id} to={`/users/${this.props.match.params.id}/investments/${investment._id}`}>
+                                    <Ticker>{investment.ticker}</Ticker>
+                                </Link>
 
-                    </Table>
+                            </div>
+                        ))}
+                    </Column>
+                    
+                    <Column>
+                    <ColumnTitle>quantity</ColumnTitle>
+                        {this.state.investments.map(investment => {
+                            return <div>{investment.quantity}</div>
+                        })}
+                    </Column>
+                    <Column>
+                    <ColumnTitle>price</ColumnTitle>
+                        {this.state.fakePrices.map(price => {
+                            return <div>{price}</div>
+                        })}
+                    </Column>
+                    <Column>
+                    <ColumnTitle>total</ColumnTitle>
+                        {this.state.fakeTotals.map(total => {
+                            return <div>{total}</div>
+                        })}
+
+                    </Column>
+
+
+                </Table>
 
                 <button onClick={this.toggleShowNewForm}>Add Investment</button>
 
@@ -57,9 +82,20 @@ class InvestmentList extends Component {
 export default InvestmentList
 
 const Column = styled.div`
-    border-right: 1px black solid;
+    /* border-right: 1px black solid; */
 `
+const ColumnTitle = styled.div`
+    padding-bottom: 8px;
+
+`
+
 const Table = styled.div`
     border: 1px black solid;
     padding: 2px;
+    display: flex;
+    justify-content: space-between;
+`
+const Ticker = styled.div`
+    color: black;
+    text-decoration: none;
 `
