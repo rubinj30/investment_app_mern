a295f31c244c684cee48aae986b610639d50c84c
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-02-27 13:10:06 -0500

    updated seeds to include purchase price of stock

--- db/seeds.js
+++ db/seeds.js
@@ -15,25 +15,31 @@ User.remove({}).then(() => {
         username: 'ScroogeMcDuck',
         email: 'scrooge@mcduck.com',
         age: '56',
-        price: 0
     })
     const amzn = new Investment({
         ticker: 'AMZN' ,
         type: 'stock',
         quantity: 10,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 320
     })
     const fb = new Investment({
         ticker: 'FB' ,
         type: 'stock',
-        quantity: 6,
-        price: 0
+        quantity: 23,
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 97.24
+        
     })
     const hd = new Investment({
         ticker: 'HD' ,
         type: 'stock',
         quantity: 8,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 153.32
     })
     // const ltc = new Investment({
     //     ticker: 'LTC',
@@ -49,25 +55,33 @@ User.remove({}).then(() => {
         ticker: 'TSLA',
         type: 'stock',
         quantity: 14,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 342.12
     })
     const dal = new Investment({
         ticker: 'DAL',
         type: 'stock',
         quantity: 1020,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 1333,
+        stockPurchasePrice: 45.32
     })
     const ll = new Investment({
         ticker: 'LL',
         type: 'stock',
-        quantity: 1525,
-        price: 0
+        quantity: 700,
+        price: 0,
+        totalPurchasePrice: 12551,
+        stockPurchasePrice: 17.93
     })
     const twlo = new Investment({
         ticker: 'TWLO',
         type: 'stock',
-        quantity: 2000,
-        price: 0
+        quantity: 100,
+        price: 0,
+        totalPurchasePrice: 28.33,
+        stockPurchasePrice: 2833.00
     })
 
     user1.investments.push(amzn, fb, hd,  tsla, dal, ll, twlo)
@@ -76,20 +90,23 @@ User.remove({}).then(() => {
     const user2 = new User({
         username: 'warren_buffett',
         email: 'warren@berkshire.com',
-        age: '60',
-        price: 0
+        age: '60'
     })
     const amzn = new Investment({
         ticker: 'AMZN' ,
         type: 'stock',
         quantity: 5,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 320.43
     })
     const fb = new Investment({
         ticker: 'FB' ,
         type: 'stock',
         quantity: 6,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 140.12
     })
     // const doge = new Investment({
     //     ticker: 'DOGE' ,
@@ -100,19 +117,25 @@ User.remove({}).then(() => {
         ticker: 'JNJ' ,
         type: 'stock',
         quantity: 20,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 132.42
     })
     const nke = new Investment({
         ticker: 'NKE' ,
         type: 'stock',
         quantity: 40,
-        price: 0
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 78.44
     })
     const ibm = new Investment({
         ticker: 'IBM',
         type: 'stock',
-        quantity: 30,
-        price: 0
+        quantity: 12,
+        price: 0,
+        totalPurchasePrice: 0,
+        stockPurchasePrice: 194.23
     })
     user2.investments.push(amzn, fb, jnj, nke, ibm)
     return user2.save()
